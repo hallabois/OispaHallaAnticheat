3406459cb0577a22faa7fd742770ae66c9d553a2
Elias Eskelinen <elias.eskelinen@pm.me>
2022-03-09 22:43:40 +0200

    move argument handling to clap

--- src/main.rs
+++ src/main.rs
@@ -63,6 +63,11 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 use std::env;
+
+// We'll let clap handle argument parsing
+use clap::{Parser, CommandFactory};
+
+// read_input provides an easy way to handle typed user input
 #[cfg(feature = "game")]
 use read_input::prelude::*;
 
@@ -89,101 +94,39 @@ mod server;
 #[allow(dead_code)]
 const DEBUG_INFO: bool = false;
 
-/// Virheenkorjaustiedoissa näytettävä ohjelman nimi
-const NAME: &str = "OispaHallaAnticheat";
-
-/// Virheenkorjaustiedoissa näytettävä palvelimen nimi
-#[cfg(feature = "server")]
-const NAME_SERVER: &str = "HAC";
-
-/// Virheenkorjaustiedoissa näytettävä ohjelman versio
-const VERSION: &str = env!("CARGO_PKG_VERSION");
-
-/// Tulostaa ohjeet ohjelman suorittamiseen
-fn give_help() {
-    println!("{} {}", NAME, VERSION);
+/// An anticheat solution for the 2048 fork OispaHalla
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
     #[cfg(feature = "server")]
-    println!("\t--server{}starts a webserver for {}", "\t".repeat(4), NAME_SERVER);
+    /// starts the api server
+    #[clap(short, long)]
+    server: bool,
+
     #[cfg(feature = "game")]
-    println!("\t--game{}starts an interactive game of 2048", "\t".repeat(5));
-    println!("\t--benchmark [rounds]{}starts a benchmark", "\t".repeat(3));
-    println!("\t--analyze [game]{}prints the game step by step and validates it.", "\t".repeat(3));
+    /// starts an interactive game of 2048
+    #[clap(short, long)]
+    game: bool,
+
+    /// starts a benchmark
+    #[clap(short, long, name = "rounds")]
+    benchmark: Option<usize>,
+
+    /// parses and prints the game step by step and then validates it.
+    #[clap(short, long, name = "game")]
+    analyze: Option<String>,
+
     #[cfg(feature = "bot")]
-    println!("\t--hack [max score] [board size]{}plays the game by itself.", "\t".repeat(2));
-    println!("\t--help{}shows this info", "\t".repeat(5));
+    /// plays the game by itself.
+    #[clap(short, long, name = "max score")]
+    hack: Option<usize>
 }
 
+#[allow(unreachable_code)]
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    println!("Arguments: {:?}", args);
-    let enable_server = args.contains(&"--server".to_owned());
-    let game = args.contains(&"--game".to_owned());
-    let benchmark = args.contains(&"--benchmark".to_owned());
-    let mut benchmark_rounds = 1000;
-    let analyze = args.contains(&"--analyze".to_owned());
-    let mut analyze_data = "";
-    let do_hack = args.contains(&"--hack".to_owned());
-    #[cfg(feature = "bot")]
-    let mut hack_stack_size: usize = usize::MAX;
-    #[cfg(feature = "bot")]
-    let mut hack_board_size: usize = 4;
-    #[cfg(feature = "bot")]
-    let mut hack_max_score: usize = 10000;
-    #[cfg(feature = "bot")]
-    let mut hack_mode = 0;
-    #[cfg(feature = "bot")]
-    if do_hack && args.len() > 2{
-        hack_max_score = args[2].parse::<usize>().unwrap();
-    }
-    #[cfg(feature = "bot")]
-    if do_hack && args.len() > 3{
-        hack_board_size = args[3].parse::<usize>().unwrap();
-    }
-    #[cfg(feature = "bot")]
-    if do_hack && args.len() > 4{
-        hack_stack_size = args[4].parse::<usize>().unwrap();
-    }
-    #[cfg(feature = "bot")]
-    if do_hack && args.len() > 5{
-        hack_mode = args[5].parse::<usize>().unwrap();
-    }
-    if benchmark && args.len() == 3{
-        benchmark_rounds = args[2].parse::<usize>().unwrap();
-    }
-    if analyze && args.len() == 3{
-        analyze_data = args[2].as_str();
-    }
-    let help = args.contains(&"--help".to_owned()) || !(enable_server || benchmark || game || analyze || do_hack);
+    let args = Args::parse();
 
-    if help{
-        give_help();
-        return;
-    }
-
-    if analyze {
-        let parsed = parse_data(analyze_data.to_owned());
-        println!("Loaded record wit the length of {}.", parsed.history.len());
-        println!("History:");
-        let mut index: usize = 0;
-        for i in &parsed.history{
-            println!("History at index {}:", index);
-            board::print_board(i.0, parsed.width, parsed.height);
-            println!("move to direction {:?} and add {:?}", i.1, i.2);
-            index += 1;
-        }
-        println!( "{:#?}", validator::validate_history( parsed ) );
-        let parsed2 = parse_data(analyze_data.to_owned());
-        println!( "Run score: {}", validator::get_run_score(&parsed2) );
-        return;
-    }
-
-    #[cfg(feature = "bot")]
-    if do_hack {
-        hack(hack_stack_size, hack_max_score, hack_board_size, hack_mode);
-        return;
-    }
-
-    if benchmark {
+    if let Some(benchmark_rounds) = args.benchmark {
         println!("Benchmarking with {} rounds:", benchmark_rounds);
         for _i in 0..benchmark_rounds{
             let data = "0.0.0.0.0.0.0.0.0.2.0.0.2.0.0.0+3,0.2;1:0.0.0.2.0.0.0.0.0.0.0.2.0.0.0.2+3,3.2;0:0.0.0.4.0.0.0.2.0.0.0.0.0.0.0.2+2,0.2;3:4.0.2.0.2.0.0.0.0.0.0.0.2.0.0.0+0,1.2;1:0.0.4.2.2.0.0.2.0.0.0.0.0.0.0.2+2,0.4;2:0.0.4.0.0.0.0.0.0.0.0.2.2.0.4.4+1,0.2;3:4.2.0.0.0.0.0.0.2.0.0.0.2.8.0.0+1,0.2;1:0.2.4.2.0.0.0.0.0.0.0.2.0.0.2.8+3,1.2;2:0.0.0.0.0.0.0.2.0.0.4.4.0.2.2.8+2,0.2;3:0.0.2.0.2.0.0.0.8.0.0.0.4.8.0.0+0,1.2;1:0.0.0.2.2.0.0.2.0.0.0.8.0.0.4.8+3,1.2;3:2.0.0.0.4.0.0.2.8.0.0.0.4.8.0.0+0,3.2;1:0.0.0.2.0.0.4.2.0.0.0.8.2.0.4.8+1,3.2;0:2.0.8.4.0.0.0.16.0.0.0.0.0.2.0.0+2,2.2;3:2.8.4.0.16.0.0.0.0.0.2.0.2.0.0.0+1,1.2;1:0.2.8.4.0.2.0.16.0.0.0.2.0.0.0.2+2,1.4;3:2.8.4.0.2.16.4.0.2.0.0.0.2.0.0.0+2,2.4;1:0.2.8.4.0.2.16.4.0.0.4.2.0.0.0.2+3,1.2;2:0.0.0.0.0.0.8.2.0.0.16.8.0.4.4.4+1,3.2;0:0.4.8.2.0.0.16.8.0.0.4.4.0.2.0.0+3,1.2;3:4.8.2.0.16.8.0.2.8.0.0.0.2.0.0.0+0,2.2;1:0.4.8.2.0.16.8.2.2.0.0.8.0.0.0.2+0,3.2;0:2.4.16.4.0.16.0.8.0.0.0.2.2.0.0.0+2,2.2;3:2.4.16.4.16.8.0.0.2.0.2.0.2.0.0.0+0,3.2;1:2.4.16.4.0.0.16.8.0.0.0.4.2.0.0.2+1,3.2;1:2.4.16.4.0.0.16.8.0.0.0.4.0.2.0.4+0,2.4;0:2.4.32.4.0.2.0.8.4.0.0.8.0.0.0.0+2,2.2;3:2.4.32.4.2.8.0.0.4.8.2.0.0.0.0.0+0,3.2;0:4.4.32.4.4.16.2.0.0.0.0.0.2.0.0.0+3,2.2;1:0.8.32.4.0.4.16.2.0.0.0.2.0.0.0.2+0,0.2;2:2.0.0.0.0.0.0.4.0.8.32.2.0.4.16.4+1,0.2;3:2.2.0.0.4.0.0.0.8.32.2.0.4.16.4.0+0,2.2;1:0.0.0.4.0.0.0.4.2.8.32.2.0.4.16.4+2,0.2;2:0.0.2.0.0.0.0.8.0.8.32.2.2.4.16.4+0,0.2;1:2.0.0.2.0.0.0.8.0.8.32.2.2.4.16.4+0,1.2;2:0.0.0.2.2.0.0.8.0.8.32.2.4.4.16.4+3,3.2;3:2.0.0.0.2.8.0.0.8.32.2.0.8.16.4.2+0,2.2;1:0.0.0.2.0.0.2.8.2.8.32.2.8.16.4.2+0,3.2;0:2.8.2.2.8.16.32.8.0.0.4.4.2.0.0.0+2,3.2;3:2.8.4.0.8.16.32.8.8.0.0.0.2.0.2.0+1,3.2;1:0.2.8.4.8.16.32.8.0.0.0.8.0.2.0.4+3,2.2;3:2.8.4.0.8.16.32.8.8.0.0.2.2.4.0.0+0,2.2;1:0.2.8.4.8.16.32.8.2.0.8.2.0.0.2.4+2,3.2;3:2.8.4.0.8.16.32.8.2.8.2.0.2.4.2.0+3,2.2;0:2.8.4.8.8.16.32.0.4.8.4.2.0.4.0.0+2,3.2;1:2.8.4.8.0.8.16.32.4.8.4.2.0.0.2.4+0,0.2;2:2.0.4.8.0.0.16.32.2.8.4.2.4.16.2.4+3,0.2;3:2.4.8.2.16.32.0.0.2.8.4.2.4.16.2.4+1,1.2;1:2.4.8.2.0.2.16.32.2.8.4.2.4.16.2.4+0,0.4;2:4.4.8.2.0.2.16.32.4.8.4.2.4.16.2.4+3,0.2;3:8.8.2.2.2.16.32.0.4.8.4.2.4.16.2.4+0,1.2;1:0.0.16.4.2.2.16.32.4.8.4.2.4.16.2.4+0,2.4;0:2.2.32.4.8.8.4.32.4.16.2.2.0.0.0.4+3,2.2;3:4.32.4.0.16.4.32.0.4.16.4.2.4.0.0.0+0,0.2;1:2.4.32.4.0.16.4.32.4.16.4.2.0.0.0.4+2,3.2;0:2.4.32.4.4.32.8.32.0.0.0.2.0.0.2.4+1,2.4;3:2.4.32.4.4.32.8.32.2.4.0.0.2.4.0.0+1,3.2;1:2.4.32.4.4.32.8.32.0.0.2.4.0.2.2.4+0,3.2;0:2.4.32.4.4.32.8.32.0.2.4.8.2.0.0.0+2,3.4;3:2.4.32.4.4.32.8.32.2.4.8.0.2.0.4.0+0,3.2;1:2.4.32.4.4.32.8.32.0.2.4.8.2.0.2.4+2,3.2;3:2.4.32.4.4.32.8.32.2.4.8.0.4.4.2.0+0,3.2;1:2.4.32.4.4.32.8.32.0.2.4.8.2.0.8.2+0,0.2;2:2.0.32.4.2.4.8.32.4.32.4.8.2.2.8.2+0,0.4;2:4.0.32.4.4.4.8.32.4.32.4.8.2.2.8.2+0,3.2;1:0.4.32.4.0.8.8.32.4.32.4.8.2.4.8.2+0,1.2;1:0.4.32.4.2.0.16.32.4.32.4.8.2.4.8.2+0,0.2;2:2.0.32.4.2.4.16.32.4.32.4.8.2.4.8.2+0,0.4;f".to_owned();
@@ -193,39 +136,57 @@ fn main() {
         println!("Done!");
         return;
     }
-    #[cfg(feature = "game")]
-    if game{
-        let mut board: Board = Board{
-            tiles: board::create_tiles(4, 4),
-            width: 4,
-            height: 4
-        };
-        board.set_tile(0, 0, 2);
-        board.set_tile(1, 0, 2);
-        board.set_tile(3, 1, 2);
-        board::print_board(board.tiles, 4, 4);
-        println!("input \"9\" to exit.");
-        loop {
-            let inp = input::<usize>().get();
-            if inp == 9{
-                break;
-            }
-            let dir = [Direction::UP, Direction::RIGHT, Direction::DOWN, Direction::LEFT, Direction::END][inp];
-            let next = board::is_move_possible(board, dir);
-            if next.1 {
-                println!("Next state: ");
-                board::print_board(next.0, 4, 4);
-            }
-            else {
-                println!("Move not possible!")
-            }
-            board.tiles = next.0;
-        }
+    #[cfg(feature = "server")]
+    if args.server {
+        println!("Starting the api server...");
+        server::start_server();
         return;
     }
-    #[cfg(feature = "server")]
-    if enable_server{
-        println!("Start the web server:");
-        server::start_server();
+    #[cfg(feature = "game")]
+    if args.game {
+        game();
+        return;
     }
+    #[cfg(feature = "bot")]
+    if let Some(hack_max_score) = args.hack {
+        let hack_stack_size = 400;
+        let hack_board_size = 4;
+        let hack_mode = 0;
+        hack(hack_stack_size, hack_max_score, hack_board_size, hack_mode);
+        return;
+    }
+
+    // Print help if no arguments are supplied
+    let _ = Args::command().print_long_help();
+}
+
+#[cfg(feature = "game")]
+fn game() {
+    let mut board: Board = Board{
+        tiles: board::create_tiles(4, 4),
+        width: 4,
+        height: 4
+    };
+    board.set_tile(0, 0, 2);
+    board.set_tile(1, 0, 2);
+    board.set_tile(3, 1, 2);
+    board::print_board(board.tiles, 4, 4);
+    println!("input \"9\" to exit.");
+    loop {
+        let inp = input::<usize>().get();
+        if inp == 9{
+            break;
+        }
+        let dir = [Direction::UP, Direction::RIGHT, Direction::DOWN, Direction::LEFT, Direction::END][inp];
+        let next = board::is_move_possible(board, dir);
+        if next.1 {
+            println!("Next state: ");
+            board::print_board(next.0, 4, 4);
+        }
+        else {
+            println!("Move not possible!")
+        }
+        board.tiles = next.0;
+    }
+    return;
 }
